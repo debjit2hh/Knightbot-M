f3bd0ad0c1f68bbe349362d550da01fc21024e85
debjit2hh <dasanita17171@gmail.com>
2025-06-04 21:19:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'BERLIN SIR', // Your name
-  ownerNumber: '919946652925', //Your number
+  ownerNumber: '917439160237', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
