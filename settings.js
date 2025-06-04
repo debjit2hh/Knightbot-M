e19cb64ce1a0f042f086dfc2cef822223b3fa2b0
debjit2hh <dasanita17171@gmail.com>
2025-06-04 21:02:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '923004836607', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Deb', // Your name
+  ownerNumber: '917439160237', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
